26abc22e2d643d7661314ca257327e036fa2aec3
Ahmet Kaleli <ahmetkaleli61@gmail.com>
2025-02-25 00:08:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import app from "./services/Firebase"
+export default function App() {
+  console.log("Firebase initialized:", app);
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
